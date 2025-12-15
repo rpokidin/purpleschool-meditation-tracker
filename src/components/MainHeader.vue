06827a2b144b53fea09571a60d207eb31e208b79
rpokidin <rpokidin@yandex.ru>
2025-12-15 17:48:47 +0300

    cmt 10-vue-router

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="header">
+    <div class="logo">
+      <img src="../assets/logo.svg" alt="">
+    </div>
+    <nav class="nav">
+      <ul>
+        <li>
+          <RouterLink to="/">
+            <div class="nav__ico">
+              <img src="../assets/nav/play.svg" alt="">
+            </div>
+            <div class="nav__name">Медитация</div>
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/statistics" active-class="active">
+            <div class="nav__ico">
+              <img src="../assets/nav/chart.svg" alt="">
+            </div>
+            <div class="nav__name">Статистика</div>
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/">
+            <div class="nav__ico">
+              <img src="../assets/nav/exit.svg" alt="">
+            </div>
+            <div class="nav__name">Выход</div>
+          </RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 200px;
+}
+.logo {
+  height: 180px;
+}
+.nav ul {
+  display: flex
+}
+.nav ul li {
+  padding: 32px 60px;
+}
+.nav ul li:not(:last-child) {
+  border-right: 3px solid rgba(58, 80, 81, 1)
+}
+.nav ul li a {
+  text-align: center;
+  opacity: .5;
+  color: var(--white);
+}
+.nav__name {
+  font-size: 22px;
+}
+.nav ul li a:hover,
+.nav ul li a.active {
+  opacity: 1;
+}
+</style>
+
+
---
+++ src/components/MainHello.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <img class="profile-pic" src="../assets/profile.png" alt="">
+    <h1>Добро пожаловать, Наталья!</h1>
+    <p>Как вы сегодня себя чувствуете?</p>
+    <ul class="meditaion-list">
+      <li>
+        <button>
+          <div class="meditaion-list__ico">
+            <img src="../assets/meditation/calm.svg" alt="">
+          </div>
+          <div class="meditaion-list__name">Спокойно</div>
+        </button>
+      </li>
+      <li>
+        <button>
+          <div class="meditaion-list__ico">
+            <img src="../assets/meditation/relax.svg" alt="">
+          </div>
+          <div class="meditaion-list__name">Расслабленно</div>
+        </button>
+      </li>
+      <li>
+        <button>
+          <div class="meditaion-list__ico">
+            <img src="../assets/meditation/focus.svg" alt="">
+          </div>
+          <div class="meditaion-list__name">Фокусировано</div>
+        </button>
+      </li>
+      <li>
+        <button>
+          <div class="meditaion-list__ico">
+            <img src="../assets/meditation/anxious.svg" alt="">
+          </div>
+          <div class="meditaion-list__name">Тревожно</div>
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.profile-pic {
+  margin-bottom: 30px;
+}
+h1 {
+  font-weight: 500;
+  font-size: 30px;
+  margin-bottom: 2px;
+}
+p {
+  font-size: 22px;
+  color: var(--white-700);
+}
+.meditaion-list {
+  display: flex;
+  gap: 20px;
+  margin-top: 40px;
+}
+.meditaion-list button {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.meditaion-list__ico {
+  background-color: var(--white);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 65px;
+  height: 65px;
+  margin-bottom: 5px;
+  border-radius: 20px;
+}
+.meditaion-list button:hover .meditaion-list__ico {
+  background-color: #69B09C;
+}
+.meditaion-list__name {
+  font-size: 12px;
+  color: var(--white);
+}
+</style>
+
+
